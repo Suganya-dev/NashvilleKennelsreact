601abf6ad6cbc5abb4b13a17e339217a88b2f873
Suganya <suganprakash0621@gmail.com>
2020-11-26 23:06:18 -0600

    created auth component

--- src/components/kennel.js
+++ src/components/kennel.js
@@ -1,5 +1,10 @@
 import React from "react"
-import { Route } from "react-router-dom"
+import { Route, Redirect } from "react-router-dom"
+import { ApplicationViews } from "./ApplicationViews"
+import { NavBar } from "./nav/NavBar"
+import { Login } from "./auth/Login"
+import { Register } from "./auth/Register"
+import "./Kennel.css"
 import "./animal/Animal.css"
 import "./employee/Employee.css"
 import "./customers/Customer.css"
@@ -7,59 +12,24 @@ import "./location/Location.css"
 import "./animal/Animal.css"
 import "./customers/Customer.css"
 import "./employee/Employee.css"
-
-import {LocationList} from "./location/LocationList"
-import {LocationProvider} from "./location/LocationProvider"
-import {AnimalProvider} from "./animal/AnimalProvider"
-import{AnimalList} from "./animal/AnimalList"
-import{CustomerProvider} from "./customers/CustomerProvider"
-import{CustomerList} from "./customers/CustomerList"
-import{EmployeeProvider} from "./employee/EmployeeProvider"
-import {EmployeeList} from"./employee/EmployeeList"
-import { NavBar } from "./nav/NavBar"
-import { ApplicationViews } from "./ApplicationViews"
 import "./kennel.css"
 
 export const Kennel = () => (
     <>
-        <NavBar />
-        <ApplicationViews />
+        <Route render={() => {
+            if (localStorage.getItem("kennel_customer")) {
+            return (
+            <>
+            <Route render={props => <NavBar {...props} />} />
+            <Route render={props => <ApplicationViews {...props} />} />
+             </>
+            )
+            } else {
+                return <Redirect to="/login" />
+            }
+        }} />
+
+    <Route path="/login" render={props => <Login {...props} />} />
+    <Route path="/register" render={props => <Register {...props} />} />
     </>
 )
-// export const Kennel = () => (
-//     <>
-//         <h2>Nashville Kennels</h2>
-//         <small>Loving care when you're not there.</small>
-//         <address>
-//             <div>Visit Us at the Nashville North Location</div>
-//             <div>500 Puppy Way</div>
-//         </address>
-//         <h2>Animals</h2>
-//         <article className="animals">
-
-//             <AnimalProvider >
-//             <AnimalList/>
-//             </AnimalProvider >
-//              </article>
-
-//         <h2>Employees</h2>
-//         <article className="employees">
-//             <EmployeeProvider >
-//             <EmployeeList />
-//             </EmployeeProvider >
-//         </article>
-//         <h2>Customers</h2>
-//         <article className="customers">
-//             < CustomerProvider>
-//             <CustomerList/>
-//             </ CustomerProvider>
-            
-//         </article>
-//         <h2>Location</h2>
-//         <article className="locations">
-//         <LocationProvider>
-//             < LocationList/>
-//             </LocationProvider>
-//             </article>
-//     </>
-// )
